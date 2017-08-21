cb6c08cf358b264e9bab3243d7ce0c5f96777eb3
foory <foory@139.com>
2017-08-21 11:34:37 +0800

    xxx

--- ejsIndex/routes/index.js
+++ ejsIndex/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '你是傻逼' });
 });
 
 module.exports = router;
